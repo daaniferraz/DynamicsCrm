735c8579cabe5d581eabd48b56d8ced7d340faf8
Daniel Rodrigues Ferraz <daniel_rodrigues_ferraz@hotmail.com>
2021-07-28 11:07:09 -0300

    Importação Conta- Daniel Ferraz

---
+++ Dynamics CRM/ConexaoImport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace Dynamics_CRM
+{
+    class ConexaoImport
+    {
+        private static CrmServiceClient crmServiceClientDestino;
+
+        public CrmServiceClient Obter()
+        {
+            var connectionStringCRM = @"AuthType=OAuth;
+            Username = Daaniferraz@danielrodriguesferraz.onmicrosoft.com;
+            Password = Segredo#1127; SkipDiscovery = True;
+            AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
+            RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
+            Url = https://org44041492.crm2.dynamics.com/main.aspx;";
+
+
+            if (crmServiceClientDestino == null)
+            {
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                crmServiceClientDestino = new CrmServiceClient(connectionStringCRM);
+            }
+            return crmServiceClientDestino;
+        }
+    }
+}
---
+++ Dynamics CRM/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamics_CRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CrmImport = new ConexaoImport().Obter();
+            var CrmNew = new ConexaoCrm().Obter();
+
+            ImportarContaCrmTerceiro(CrmImport);
+
+            Console.WriteLine("Fim de Execução");
+
+        }
+
+        #region Importar
+
+        static void ImportarContaCrmTerceiro(CrmServiceClient CrmImport)
+
+        {
+            string query = @"<fetch version='1.0' output-format='xml-plataform' mapping='logical' distinct='false'>
+                            <entity name='account'>
+                                <attribute name='name' />
+                                <attribute name='primarycontactid' />
+                                <attribute name='telephone1' />
+                                <attribute name='accountid' />
+                                <attribute name='createdon' />
+                                <attribute name='emailaddress1'/>
+                                <order attribute='name' descending='false' />
+                                <filter type='and'>
+                                    <condition attribute='name' operator='not-null' />
+                                    
+                                </filter>
+                            </entity>
+                            </fetch>";
+            EntityCollection colecao = CrmImport.RetrieveMultiple(new FetchExpression(query));
+            var conection = new ConexaoCrm().Obter();
+
+            foreach (var item in colecao.Entities)
+            {
+                var entidade = new Entity("account");
+                
+                Guid registro = new Guid();
+
+                var idImport = item.Id;
+
+                entidade.Attributes.Add("name",item["name"].ToString());
+
+                
+                if (CrmImport.Retrieve("account", idImport, new ColumnSet("telephone1")).Attributes.Contains("telephone1"))
+                {
+                    entidade.Attributes.Add("telephone1", item["telephone1"].ToString());
+                }
+                else
+                    entidade.Attributes.Add("telephone1", "".ToString());
+
+                //entidade.Attributes.Add("emailaddress1", item["emailaddress1"].ToString());
+                //entidade.Attributes.Add("accountid", item["accountid"]);
+                
+                registro = conection.Create(entidade);
+            }
+
+        }
+
+        #endregion
+
+    }
+}
