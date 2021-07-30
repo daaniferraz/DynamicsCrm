4af2863e6bfa191181608ac1b3ec2e44a7e7a1a9
ErickNeves <34195145+ErickNeves@users.noreply.github.com>
2021-07-30 20:29:31 -0300

    Criação do Plugin Duplicata CPF

---
+++ Plugins/Contact_CpfPreValidation.cs
@@ -0,0 +1,51 @@
+﻿﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using PluginStepValidation;
+
+namespace ContactPreValidation
+{
+    public class Contact_CpfPreValidation : IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (context.MessageName.ToLower() == "create" || context.MessageName.ToLower() == "update" && context.Mode == Convert.ToInt32(PluginStepEnum.Mode.Synchronous) &&
+                context.Stage == Convert.ToInt32(PluginStepEnum.Stage.PreValidation))
+            {
+
+                var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                var service = serviceFactory.CreateOrganizationService(context.UserId);
+                var trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+                trace.Trace("Iniciando o Plugin para validar se o CPF está duplicado.");
+
+                Entity CrmTargetEntity = null;
+
+                if (context.InputParameters.Contains("Target"))
+                {
+                    CrmTargetEntity = (Entity)context.InputParameters["Target"];
+                }
+
+
+                trace.Trace("Entidade atribuida ao CrmTargetEntity");
+
+                QueryExpression queryExpression = new QueryExpression("contact");
+                queryExpression.Criteria.AddCondition("grp3_cpf", ConditionOperator.Equal, CrmTargetEntity.Attributes["grp3_cpf"].ToString());
+                queryExpression.ColumnSet = new ColumnSet("grp3_cpf");
+                EntityCollection colecaoEntidades = service.RetrieveMultiple(queryExpression);
+
+
+                if (colecaoEntidades.Entities.Count > 0)
+                {
+                    throw new InvalidPluginExecutionException("CPF já cadastrado. Por favor insira um CPF ainda não cadastrado.");
+                }
+            }
+
+        }
+
+    }
+}
