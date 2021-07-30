20ed06fa67295a67c5acee8edde3d138777f286b
Daniel Rodrigues Ferraz <daniel_rodrigues_ferraz@hotmail.com>
2021-07-29 23:06:15 -0300

    Validação Campos NULL

--- Dynamics CRM/ImportacaoConta.cs
+++ Dynamics CRM/ImportacaoConta.cs
@@ -10,14 +10,25 @@ using System.Threading.Tasks;
 namespace Dynamics_CRM
 {
     class ImportacaoConta
+        
     {
-        public  void ImportarConta(CrmServiceClient CrmImport)
+        public void ImportarConta(CrmServiceClient CrmImport)
 
         {
             string query = @"<fetch version='1.0' output-format='xml-plataform' mapping='logical' distinct='true' >
                             <entity name='account'>
                              <attribute name='name' />
                              <attribute name='drf_cpfcnpj' />
+                             <attribute name='telephone1' />
+                             <attribute name='address1_line1' />
+                             <attribute name='address1_postalcode' />
+                             <attribute name='address1_city' />
+                             <attribute name='address1_stateorprovince' />
+                             <attribute name='address1_country' />
+                             <attribute name='creditlimit' />
+                             <attribute name='transactioncurrencyid' />
+                             <attribute name='emailaddress1' />
+                                   
                              </entity>
                             </fetch>";
 
@@ -28,21 +39,38 @@ namespace Dynamics_CRM
             {
                 try
                 {
+
+                    
                     var entidade = new Entity("account");
 
                     Guid registro = new Guid();
 
-                    var idImport = item.Id;
+                    //var idImport = item["accountid"];
+                    var temp = item.Id;
+
+                    
 
                     var nome = item["name"].ToString();
                     var cpf = item["drf_cpfcnpj"].ToString();
+                    string tel = "telephone1";
+                    //var add1 = item["address1_line1"].ToString();
+                    //var post = item["address1_postalcode"].ToString();
+                    //var city = item["address1_city"].ToString();
+                    //var state = item["address1_stateorprovince"].ToString();
+                    //var contry = item["address1_country"].ToString();
+                    //var credt = item["creditlimit"].ToString();
+                    //var money = item["transactioncurrencyid"].ToString();
+                    //var email = item["emailaddress1"].ToString();
 
 
 
                     string query2 = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
                                 <entity name='account'>
-                                    <attribute name='name'/>
-                                    <attribute name='grp3_cpfcnpj'/>
+
+                                   <attribute name='name' />
+                                   <attribute name='grp3_cpfcnpj' />
+                                   
+
                                     <order attribute='name' descending='false' />
                                     <filter type='and'>
                                         <condition attribute='name' operator='eq' value= '{0}'/>
@@ -50,15 +78,33 @@ namespace Dynamics_CRM
                                     </filter>
                                 </entity>
                             </fetch>";
-                    query2 = string.Format(query2, nome.ToString(),cpf.ToString());
-                    
+                    query2 = string.Format(query2, nome.ToString(), cpf.ToString());
+
                     EntityCollection col = conection.RetrieveMultiple(new FetchExpression(query2));
-
-
-                    if (col.Entities.Count==0)
+                   
+                    if (col.Entities.Count == 0)
                     {
+                       
+
                         entidade.Attributes.Add("name", item["name"].ToString());
                         entidade.Attributes.Add("grp3_cpfcnpj", item["drf_cpfcnpj"].ToString());
+                      
+                        if (item.Attributes.Contains(tel) )
+                        {
+                            entidade.Attributes.Add(tel, item[tel].ToString());
+                        }
+                        //else
+                        //    entidade.Attributes.Add("telephone1","".ToString());
+                        //entidade.Attributes.Add("websiteurl", item["websiteurl"].ToString());
+
+                        entidade.Attributes.Add("address1_line1", item["address1_line1"].ToString());
+                        entidade.Attributes.Add("address1_postalcode", item["address1_postalcode"].ToString());
+                        entidade.Attributes.Add("address1_city", item["address1_city"].ToString());
+                        entidade.Attributes.Add("address1_stateorprovince", item["address1_stateorprovince"].ToString());
+                        entidade.Attributes.Add("address1_country", item["address1_country"].ToString());
+                        //entidade.Attributes.Add("creditlimit", item["creditlimit"]);
+                        //entidade.Attributes.Add("transactioncurrencyid", item["transactioncurrencyid"]);
+                        entidade.Attributes.Add("emailaddress1", item["emailaddress1"].ToString());
                         registro = conection.Create(entidade);
 
                     }
@@ -67,29 +113,21 @@ namespace Dynamics_CRM
 
                         throw new Exception();
                     }
-                    
+
                 }
                 catch (Exception)
                 {
 
                     Console.WriteLine("Log");
                 }
-               
-                
-
-                //if (!(CrmImport.Retrieve("account", idImport, new ColumnSet("drf_cpfcnpj")).Attributes.Contains("drf_cpfcnpj")))
-                //{
-
-                //    entidade.Attributes.Add("grp3_cpfcnpj", "".ToString());
-
-                //}
-                //else
-                //    entidade.Attributes.Add("grp3_cpfcnpj", item["drf_cpfcnpj"].ToString());
 
 
-               
-                
-                
+
+
+
+
+
+
 
             }
 
