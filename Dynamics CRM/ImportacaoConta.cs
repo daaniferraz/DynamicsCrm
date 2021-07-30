0fe343e4ffa2f1e209549528d3f325d4ade278fe
Daniel Rodrigues Ferraz <daniel_rodrigues_ferraz@hotmail.com>
2021-07-30 18:32:26 -0300

    Conclusão classe Contas- Daniel Ferraz

--- Dynamics CRM/ImportacaoConta.cs
+++ Dynamics CRM/ImportacaoConta.cs
@@ -24,45 +24,27 @@ namespace Dynamics_CRM
                              <attribute name='address1_postalcode' />
                              <attribute name='address1_city' />
                              <attribute name='address1_stateorprovince' />
-                             <attribute name='address1_country' />
-                             <attribute name='creditlimit' />
-                             <attribute name='transactioncurrencyid' />
+                             <attribute name='address1_country' />   
                              <attribute name='emailaddress1' />
-                                   
                              </entity>
                             </fetch>";
 
             EntityCollection colecao = CrmImport.RetrieveMultiple(new FetchExpression(query));
+            
             var conection = new ConexaoCrm().Obter();
 
             foreach (var item in colecao.Entities)
             {
                 try
                 {
-
                     
                     var entidade = new Entity("account");
 
                     Guid registro = new Guid();
-
-                    //var idImport = item["accountid"];
-                    var temp = item.Id;
-
-                    
+                                       
 
                     var nome = item["name"].ToString();
                     var cpf = item["drf_cpfcnpj"].ToString();
-                    string tel = "telephone1";
-                    //var add1 = item["address1_line1"].ToString();
-                    //var post = item["address1_postalcode"].ToString();
-                    //var city = item["address1_city"].ToString();
-                    //var state = item["address1_stateorprovince"].ToString();
-                    //var contry = item["address1_country"].ToString();
-                    //var credt = item["creditlimit"].ToString();
-                    //var money = item["transactioncurrencyid"].ToString();
-                    //var email = item["emailaddress1"].ToString();
-
-
 
                     string query2 = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
                                 <entity name='account'>
@@ -78,33 +60,34 @@ namespace Dynamics_CRM
                                     </filter>
                                 </entity>
                             </fetch>";
+
                     query2 = string.Format(query2, nome.ToString(), cpf.ToString());
 
                     EntityCollection col = conection.RetrieveMultiple(new FetchExpression(query2));
-                   
+
+                    string tel = "telephone1";
+                    string add1 = "address1_line1";
+                    string post = "address1_postalcode";
+                    string city = "address1_city";
+                    string state = "address1_stateorprovince";
+                    string contry = "address1_country";
+                    string email = "emailaddress1";
+
                     if (col.Entities.Count == 0)
                     {
-                       
+                        var validar = new ValidateNullField();
 
                         entidade.Attributes.Add("name", item["name"].ToString());
                         entidade.Attributes.Add("grp3_cpfcnpj", item["drf_cpfcnpj"].ToString());
-                      
-                        if (item.Attributes.Contains(tel) )
-                        {
-                            entidade.Attributes.Add(tel, item[tel].ToString());
-                        }
-                        //else
-                        //    entidade.Attributes.Add("telephone1","".ToString());
-                        //entidade.Attributes.Add("websiteurl", item["websiteurl"].ToString());
 
-                        entidade.Attributes.Add("address1_line1", item["address1_line1"].ToString());
-                        entidade.Attributes.Add("address1_postalcode", item["address1_postalcode"].ToString());
-                        entidade.Attributes.Add("address1_city", item["address1_city"].ToString());
-                        entidade.Attributes.Add("address1_stateorprovince", item["address1_stateorprovince"].ToString());
-                        entidade.Attributes.Add("address1_country", item["address1_country"].ToString());
-                        //entidade.Attributes.Add("creditlimit", item["creditlimit"]);
-                        //entidade.Attributes.Add("transactioncurrencyid", item["transactioncurrencyid"]);
-                        entidade.Attributes.Add("emailaddress1", item["emailaddress1"].ToString());
+                        validar.Validation(item, entidade, tel);
+                        validar.Validation(item, entidade, add1);
+                        validar.Validation(item, entidade, post);
+                        validar.Validation(item, entidade, city);
+                        validar.Validation(item, entidade, state);
+                        validar.Validation(item, entidade, contry);
+                        validar.Validation(item, entidade, email);
+                        
                         registro = conection.Create(entidade);
 
                     }
@@ -118,16 +101,8 @@ namespace Dynamics_CRM
                 catch (Exception)
                 {
 
-                    Console.WriteLine("Log");
+                    Console.WriteLine("Erro importação");
                 }
-
-
-
-
-
-
-
-
 
             }
 
--- Dynamics CRM/ValidateNullField.cs
+++ Dynamics CRM/ValidateNullField.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Dynamics_CRM
 {
     class ValidateNullField
     {
+        public void Validation(Entity item, Entity entidade,string variavel)
+        {
+            if (item.Attributes.Contains(variavel))
+            {
+                entidade.Attributes.Add(variavel, item[variavel].ToString());
+            }
+
+        }
     }
 }
