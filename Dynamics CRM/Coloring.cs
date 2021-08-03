cb47161c1b6f6fbf3b005d3309f48872e8302e41
ErickNeves <34195145+ErickNeves@users.noreply.github.com>
2021-08-03 15:55:15 -0300

    Criação De Testes de Criação e Plugins
    Começado a criar os testes para verificar se os plugins estão ativando em criações por meio do terminal.
    Criado também uma classe para customização de cor para personalização de mensagens no terminal.

---
+++ Dynamics CRM/Coloring.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamics_CRM
+{
+    public  class Coloring
+    {
+
+        public enum Situation
+        {
+            Normal = 0,
+            Espacamento = 1,
+            Erro = 2
+            
+        }
+
+        public void ChangeColor(Situation situation)
+        {
+            switch (situation)
+            {
+                case Situation.Espacamento:
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(" ");
+                    break;
+                case Situation.Erro:
+                    //Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Situation.Normal:
+                default:
+                    Console.ResetColor();
+                    break;
+            }
+        }
+
+    }
+}
--- Dynamics CRM/Program.cs
+++ Dynamics CRM/Program.cs
@@ -22,11 +22,116 @@ namespace Dynamics_CRM
             ImportarContaCrmTerceiro.ImportarConta(CrmImport);
             importacaoPedido.ImportarPedido(CrmImport);
 
+            //TesteErick(CrmNew);
 
             Console.WriteLine("Fim de Execução");
 
+            return;
+
         }
 
-       
+
+
+        static void TesteErick(CrmServiceClient serviceProxy)
+        {
+            Coloring colors = new Coloring();
+            Guid registro = new Guid();
+
+
+            Console.WriteLine("\t:::Começando os testes de ativação de PLUGIN:::\t ");
+            
+
+            #region TesteEmContas
+            var entidade1 = new Entity("account");
+            var entidade2 = new Entity("account");
+            
+
+            
+
+            try
+            {
+                entidade1.Attributes.Add("name", "Teste 1");
+                entidade1.Attributes.Add("grp3_cpfcnpj", "11111111111111");
+                registro = serviceProxy.Create(entidade1);
+            }
+            catch (Exception ex)
+            {
+                colors.ChangeColor(Coloring.Situation.Erro);
+                Console.WriteLine("O CNPJ já existe na base de dados. Ignorando essa importação!");
+                Console.WriteLine("Nome da conta ignorada: " + entidade1.Attributes["name"].ToString());
+                Console.WriteLine("CPF: " + entidade1.Attributes["grp3_cpfcnpj"].ToString());
+                colors.ChangeColor(Coloring.Situation.Normal);
+            }
+
+            registro = new Guid();
+
+            colors.ChangeColor(Coloring.Situation.Espacamento);            
+            colors.ChangeColor(Coloring.Situation.Normal);
+
+            try
+            {
+                entidade2.Attributes.Add("name", "Teste 2");
+                entidade2.Attributes.Add("grp3_cpfcnpj", "11111111111111");
+                registro = serviceProxy.Create(entidade2);
+            }
+            catch (Exception ex)
+            {
+                colors.ChangeColor(Coloring.Situation.Erro);
+                Console.WriteLine("O CNPJ já existe na base de dados. Ignorando essa importação!");
+                Console.WriteLine("Nome da conta ignorada: " + entidade2.Attributes["name"].ToString());
+                Console.WriteLine("CPF: " + entidade2.Attributes["grp3_cpfcnpj"].ToString());
+                colors.ChangeColor(Coloring.Situation.Normal);
+                
+            }
+
+
+            #endregion
+
+            colors.ChangeColor(Coloring.Situation.Espacamento);
+            colors.ChangeColor(Coloring.Situation.Normal);
+
+            var entidadeContato1 = new Entity("contact");
+            var entidadeContato2 = new Entity("contact");
+            registro = new Guid();
+
+            try
+            {
+                entidadeContato1.Attributes.Add("firstname", "Contato de Teste");
+                entidadeContato1.Attributes.Add("lastname", "N 1");
+                entidadeContato1.Attributes.Add("grp3_cpf", "11111111111");
+                registro = serviceProxy.Create(entidadeContato1);
+            }
+            catch (Exception ex)
+            {
+                colors.ChangeColor(Coloring.Situation.Erro);
+                Console.WriteLine("O CPF já existe na base de dados. Ignorando essa importação!");
+                Console.WriteLine("Nome do contato ignorado: " + entidadeContato1.Attributes["firstname"].ToString() + " " + entidadeContato1.Attributes["lastname"].ToString());
+                Console.WriteLine("CPF: " + entidadeContato1.Attributes["grp3_cpf"].ToString());
+                colors.ChangeColor(Coloring.Situation.Normal);
+            }
+
+            registro = new Guid();
+
+            colors.ChangeColor(Coloring.Situation.Espacamento);
+            colors.ChangeColor(Coloring.Situation.Normal);
+
+            try
+            {
+                entidadeContato2.Attributes.Add("firstname", "Contato de Teste");
+                entidadeContato2.Attributes.Add("lastname", "N 2");
+                entidadeContato2.Attributes.Add("grp3_cpf", "11111111111");
+                registro = serviceProxy.Create(entidadeContato2);
+            }
+            catch (Exception ex)
+            {
+                colors.ChangeColor(Coloring.Situation.Erro);
+                Console.WriteLine("O CPF já existe na base de dados. Ignorando essa importação!");
+                Console.WriteLine("Nome do contato ignorado: " + entidadeContato2.Attributes["firstname"].ToString() + " " + entidadeContato2.Attributes["lastname"].ToString());
+                colors.ChangeColor(Coloring.Situation.Normal);
+            }
+
+        }
+
+
     }
 }
